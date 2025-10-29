c8f5bae8f9ea48161961a768d79a99a342fcd055
Patrick Thompson <patrick_thompson9@icloud.com>
2025-10-29 14:48:07 +0000

    implemented main functionality so services can be run

--- chat-server/src/main.rs
+++ chat-server/src/main.rs
@@ -1,5 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::task;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: cargo run -- <server or client>");
+        return;
+    }
+
+    match args[1].as_str() {
+        "server" => {
+            if let Err(e) = server::start_server().await {
+                eprintln!("Error starting server: {}", e)
+            }
+        }
+        "client" => {
+            if let Err(e) = client::start_client().await {
+                eprintln!("Error starting client: {}", e)
+            }
+        }
+        _ => {
+            eprintln!("Invalid argument");
+        }
+    }
 }
 
 mod server;
